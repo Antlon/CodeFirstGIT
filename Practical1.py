8f496c280b7c44903bc0f095b262425bc1b59f0b
Antlon <Antlon@users.noreply.github.com>
2019-10-16 19:43:35 +0100

    init git

---
+++ Practical1.py
@@ -0,0 +1,78 @@
+#-----------------------------------------------------
+#Practical1.py
+#-----------------------------------------------------
+
+#Exercise 1
+
+print(5-6)
+print(8*9)
+print(6/2)
+print(5/2)
+print(5.0/2)
+print(5%2)
+print(2*(10+3))
+print(2**4)
+
+#Exercise 2
+
+print("Hello "+"World!")
+print("Hello World!")
+
+#Exercise 3
+x= "HeLlo!"
+print(x.upper()) #Upper case
+print(x.lower()) #Lower case
+print(x.title()) #First letter capitalized
+
+#Exercise 4
+
+age = 5
+like = "painting"
+
+age_description = "My age is {0} and I like {1}".format(age,like)
+print(age_description)
+
+#Homework
+
+print(10/3)
+print(10.0/3.0)
+print(10%3)
+print(10.0%3.0)
+
+a = 1
+a = a+1
+print(a)
+
+b = "hello"
+c = b.title()
+print (b)
+print (c)
+
+d = "hello"
+e = d.title()
+print (d)
+print (e)
+
+name = "Dave"
+f= "Hello {0}!".format(name)
+print (f)
+
+name = "Sarah"
+f= "Hello {0}!".format(name)
+print (f)
+
+#extra Homework
+
+name = "Olivia"
+height = "178 cm"
+weight = "73 kg"
+age = 20
+eyes = "Green"
+Hair = "Blonde"
+
+print("Hey, let's talk about %s.") %s name 
+#"%" DOESN'T WORK, POSSIBLE PYTHON VER ERROR
+
+
+
+
